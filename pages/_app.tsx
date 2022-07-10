5e548cce2f826ce2599d4d6429e1cdc1872b5abb
boonruang <scubatoy@gmail.com>
2022-07-10 13:42:35 +0700

    ep46 theme font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,50 @@ import type { AppProps } from 'next/app'
 import { store } from '@/store/store'
 import { Provider } from 'react-redux'
 import { getSession } from '@/store/slices/userSlice'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { blue, red, yellow } from '@mui/material/colors'
+const drawerWidth = 240
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // update session & set token
   React.useEffect(() => {
     store.dispatch(getSession())
   }, [])
 
+  const theme = createTheme({
+    components: {
+      MuiDrawer: {
+        styleOverrides: {
+          paper: {
+            backgroundImage: 'url("/static/img/background_menu.jpg")',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'bottom',
+            width: drawerWidth,
+          },
+        },
+      },
+    },
+    typography: {
+      fontFamily: 'Roboto',
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      fontWeightBold: 700,
+    },
+    spacing: 8,
+    palette: {
+      primary: process.env.NEXT_PUBLIC_IS_PRODUCTION == '0' ? yellow : blue,
+      background: {
+        default: '#FFF',
+      },
+    },
+  })
+
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   )
 }
