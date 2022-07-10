29152d29578f5aad6996bcc7d4f97787f2f27f6e
boonruang <scubatoy@gmail.com>
2022-07-11 00:36:59 +0700

    ep53 add image in datagrid

--- pages/stock/index.tsx
+++ pages/stock/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Layout from '@/components/Layouts/Layout'
 import withAuth from '@/components/withAuth'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+import {
+  DataGrid,
+  GridColDef,
+  GridValueGetterParams,
+  GridRenderCellParams,
+} from '@mui/x-data-grid'
 import { getProducts, productSelector } from '@/store/slices/productSlice'
 import { useAppDispatch } from '@/store/store'
 import { useSelector } from 'react-redux'
+import { productImageURL } from '@/utils/commonUtil'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -17,7 +24,23 @@ const Stock = ({}: Props) => {
   }, [dispatch])
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'id', headerName: 'ID', width: 50 },
+    {
+      headerName: 'IMG',
+      field: 'image',
+      width: 80,
+      renderCell: ({ value }: GridRenderCellParams<string>) => (
+        <Image
+          height={500}
+          width={500}
+          objectFit="cover"
+          alt="product image"
+          src={productImageURL(value)}
+          style={{ width: 70, height: 70, borderRadius: '5%' }}
+        />
+      ),
+    },
+
     {
       field: 'name',
       headerName: 'Name',
