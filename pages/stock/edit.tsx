68571a1ddf5c9cff7ffca17851418620e6db1523
boonruang <scubatoy@gmail.com>
2022-07-11 02:42:22 +0700

    ep60 getServerSideProps

--- pages/stock/edit.tsx
+++ pages/stock/edit.tsx
@@ -1,7 +1,33 @@
+import Layout from '@/components/Layouts/Layout'
+import withAuth from '@/components/withAuth'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  product?: string
+}
 
-export default function edit({}: Props) {
-  return <div>stock/edit</div>
+const Edit = ({ product }: Props) => {
+  return (
+    <Layout>
+      <div>Edit {product}</div>
+    </Layout>
+  )
+}
+export default withAuth(Edit)
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const { id }: any = context.query
+  if (id) {
+    const product = id // await doGetStockById(id)
+    return {
+      props: {
+        product,
+      },
+    }
+  } else {
+    return { props: {} }
+  }
 }
